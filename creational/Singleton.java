d6cc99e6f6e8ce018c8ed4124294e43e9f822edd
Sukhdev Patil <sukhadev.patil@gmail.com>
2023-12-20 17:31:02 +0530

    first commit

---
+++ creational/Singleton.java
@@ -0,0 +1,6 @@
+
+public class Singleton {
+    public static void main(String[] args) {
+        System.out.println("hh");
+    }
+}
---
+++ src/main/java/com/core/design/pattern/CoreDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.core.design.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreDesignPatternsApplication.class, args);
+	}
+
+}
